df75b69446ebd190f54d8aa4a5cfbd1128a67f3e
Dmitriy Gonharov <my.webpage057@gmail.com>
2022-08-27 00:18:09 +0300

    add film-info-modal

--- src/js/film-info-modal.js
+++ src/js/film-info-modal.js
@@ -1,27 +1,37 @@
 export default function infoFilm() {
    const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn: document.querySelector("[data-gallery]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
    }
    refs.openModalBtn.addEventListener("click", onOpenModalBtnClick);
    refs.closeModalBtn.addEventListener("click", oncloseModalBtnClick);
    window.addEventListener("click", onWindowClick)
-
-  //  function toggleModal() {
-  //   refs.modal.classList.toggle("is-hidden");
-  //   console.log("Backdrop is hidden");
-  // }
-  
-   function onOpenModalBtnClick(){
-    refs.modal.style.display = "block"
-   }
-   function oncloseModalBtnClick() {
-   refs.modal.style.display = "none"
-   }
-   function onWindowClick(event) {
-    if(event.target === refs.modal) {
+   document.addEventListener('keydown', function(event){
+    if(event.key === "Escape"){
       refs.modal.style.display = "none"
     }
-   }
-}
+  });
+
+  //  function onOpenModalBtnClick(){
+  //     refs.modal.style.display = "block"
+  //  }
+  //  function oncloseModalBtnClick() {
+  //  refs.modal.style.display = "none"
+  //  }
+  //  function onWindowClick(event) {
+  //   if(event.target === refs.modal) {
+  //     refs.modal.style.display = "none"
+  //   }
+  //  }
+
+  // function onCardFilmClick(id, isFilm) {
+  //   if(isFilm) {
+  //     let localStorageData = [...JSON.parse(localStorage.getItem("[data-gallery]"))];
+  //     selectFilm = localStorageData.find(el => el.id === id);
+  //   } else {
+  //     selectFilm = renderCards.find(el => el.id === id);
+  //   }
+  //   showDetails(selectFilm);
+  // }
+} 
