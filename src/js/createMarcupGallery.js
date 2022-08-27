b2d6dd65aca24b94d17834337abc434ddea59911
Lavelian <98335265+Lavelian@users.noreply.github.com>
2022-08-27 09:59:41 +0300

    extension MurcupGallary

--- src/js/createMarcupGallery.js
+++ src/js/createMarcupGallery.js
@@ -1,15 +1,41 @@
-export default createMarcupGallery = data =>
-  data.reduce(
+export default createMarcupGallery = (data, flag) => {
+  localStorage.setItem('reneredCards', JSON.stringify(data));
+  if (flag) {
+    return data.reduce(
+      (
+        acc,
+        { poster_path, title, genre_ids, id, release_date = [], vote_average }
+      ) =>
+        acc +
+        `<li >
+      <button type="button" data-id="${id}" data-click>
+          <img src="https://image.tmdb.org/t/p/w500/${poster_path}"  alt="${title}"  loading="lazy">
+        </button>
+           <p class>
+             <span class=>${title}</span> </br>
+              ${genre_ids} | ${release_date.slice(
+          0,
+          4
+        )} <span>${vote_average}</span>
+             </p> 
+             
+        </li>`,
+      ''
+    );
+  }
+  return data.reduce(
     (acc, { poster_path, title, genre_ids, id, release_date = [] }) =>
       acc +
-      `<li class="gallery__item">
+      `<li >
       <button type="button" data-id="${id}" data-click>
-          <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}" class="gallery__img" loading="lazy">
+          <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}"  loading="lazy">
         </button>
-           <p class="gallery__text gallery__text--red">
-             <span class="gallery__text--black">${title}</span> </br>
+           <p class>
+             <span class=>${title}</span> </br>
               ${genre_ids} | ${release_date.slice(0, 4)}
              </p> 
+             
         </li>`,
     ''
   );
+};
